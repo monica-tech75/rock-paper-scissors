f75fbbb6605e34933ab7b2ab04718321230fab9c
monica-tech75 <moonyruna3@gmail.com>
2024-01-05 00:02:47 +0100

    change button for images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const computerChoiseDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
-const possibleChoices = document.querySelectorAll('button');
+const possibleChoices = document.querySelectorAll('img');
 let userChoice;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
